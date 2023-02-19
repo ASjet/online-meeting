c4d58f39a3be28f996e30fff79d277b2487a4ff8
Aryan Sjet <Aryan.ITFS@Gmail.com>
2023-02-19 20:52:26 +0800

    wip(video): add volume

--- src/Video.js
+++ src/Video.js
@@ -1,5 +1,6 @@
+import React, { useRef, useState } from 'react';
 import { Box } from '@mui/system';
-import React, { useEffect, useRef, useState } from 'react';
+import Slider from '@mui/material/Slider';
 import ToggleIcon from "./ToggleIcon";
 import VideocamIcon from '@mui/icons-material/Videocam';
 import VideocamOffIcon from '@mui/icons-material/VideocamOff';
@@ -9,11 +10,21 @@ import FullscreenIcon from '@mui/icons-material/Fullscreen';
 import FullscreenExitIcon from '@mui/icons-material/FullscreenExit';
 import ScreenShareOutlinedIcon from '@mui/icons-material/ScreenShareOutlined';
 import StopScreenShareOutlinedIcon from '@mui/icons-material/StopScreenShareOutlined';
+import VolumeMuteIcon from '@mui/icons-material/VolumeMute';
+import VolumeDown from '@mui/icons-material/VolumeDown';
+import VolumeUp from '@mui/icons-material/VolumeUp';
+import VolumeOffIcon from '@mui/icons-material/VolumeOff';
+
 
 function Video() {
+    const defaultVolume = 60;
+
     const frameBox = useRef();
     const localStream = useRef();
     const remoteStream = useRef();
+    const [mute, setMute] = useState(false);
+    const [volume, setVolume] = useState(defaultVolume);
+    const [volumeIcon, setVolumeIcon] = useState(VolumeUp);
 
     const [camera, setCamera] = useState(false);
     const [mic, setMic] = useState(false);
@@ -29,10 +40,12 @@ function Video() {
         } else {
             navigator.mediaDevices.getUserMedia({ video: true, audio: true })
                 .then(stream => {
+                    stream.getAudioTracks()[0].enabled = mic;
                     if (localStream.current.srcObject != null) {
                         localStream.current.srcObject.getTracks().forEach(track => track.stop());
                     }
                     localStream.current.srcObject = stream;
+                    localStream.current.muted = mute;
                 }).catch(error => {
                     alert("Failed to getUserMedia: ", error);
                 });
@@ -42,9 +55,13 @@ function Video() {
 
     function toggleMic() {
         if (mic) {
-            localStream.current.muted = true;
+            if (localStream.current.srcObject != null) {
+                localStream.current.srcObject.getAudioTracks()[0].enabled = false;
+            }
         } else {
-            localStream.current.muted = false;
+            if (localStream.current.srcObject != null) {
+                localStream.current.srcObject.getAudioTracks()[0].enabled = true;
+            }
         }
         setMic(!mic);
     }
@@ -79,6 +96,27 @@ function Video() {
         setScreenSharing(!screenSharing);
     }
 
+    const toggleVolume = (_, newVolume) => {
+        if (newVolume === 0) {
+            setVolumeIcon(VolumeMuteIcon);
+        } else if (newVolume > 0 && newVolume <= 50) {
+            setVolumeIcon(VolumeDown);
+        } else {
+            setVolumeIcon(VolumeUp);
+        }
+
+        setVolume(newVolume);
+    };
+
+    const toggleMute = () => {
+        if (mute) {
+            localStream.current.muted = false;
+        } else {
+            localStream.current.muted = true;
+        }
+        setMute(!mute);
+    };
+
     const fullscreenStyle = {
         position: "fixed",
         top: 0,
@@ -101,7 +139,6 @@ function Video() {
             <video
                 ref={remoteStream}
                 autoPlay
-                muted
                 style={{
                     width: "100%",
                     height: "100%",
@@ -126,6 +163,7 @@ function Video() {
                     ref={localStream}
                     autoPlay
                     muted
+                    volume={volume}
                     style={{
                         width: "100%",
                         height: "100%",
@@ -163,6 +201,22 @@ function Video() {
                 </div>
 
                 <div>
+                    <ToggleIcon
+                        onClick={toggleMute}
+                        toggle={mute}
+                        enable={VolumeOffIcon}
+                        disable={volumeIcon} />
+                    <Slider
+                        aria-label="Volume"
+                        value={volume}
+                        onChange={toggleVolume}
+                        style={{
+                            marginInline: "10px",
+                            padding: 0,
+                            width: "100px",
+                            color: "#FFFFFF",
+                        }}
+                    />
                     <ToggleIcon
                         onClick={toggleScreenSharing}
                         toggle={screenSharing}
