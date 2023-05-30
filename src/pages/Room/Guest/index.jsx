70125c56660d54a7688df6e3ee51176a32b0fca9
Aryan Sjet <Aryan.ITFS@Gmail.com>
2023-05-30 19:26:03 +0800

    fix(room): guest not pass chan to props

--- src/pages/Room/Guest/index.jsx
+++ src/pages/Room/Guest/index.jsx
@@ -6,5 +6,9 @@ import Video from "@/components/Video";
 export default function (props) {
   const room = useSelector((state) => state.room);
   const socketio = io.connect(room.socket_addr);
-  return <Video socket={socketio} />;
+  const reqChan = new MessageChannel();
+  const approveChan = new MessageChannel();
+  return (
+    <Video socket={socketio} reqChan={reqChan} approveChan={approveChan} />
+  );
 }
